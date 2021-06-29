42d0258c4f99bff12da2a85331fe6cd89437ee76
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-06-29 20:35:47 -0300

    Commit 9 dos arquivos

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logoImg from "../assets/images/logo.svg";
+import { Button } from '../components/Button';
+import '../styles/room.scss';
 
 export function Room() {
   return (
@@ -10,7 +12,21 @@ export function Room() {
           <div>codigo</div>
         </div>
       </header>
-      <main className="content"></main>
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+        <form>
+          <textarea
+            placeholder="O que voce quer perguntar?"
+          />
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça o seu login.</button></span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
